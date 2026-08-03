6f0f80660dbad1ddcba124f84ebc559b85653310
rishi-vinod <rishivinod08@gmail.com>
2026-08-03 11:08:30 -0400

    feat: runvcgencmd function

--- test_vcgencmd.py
+++ test_vcgencmd.py
@@ -1,8 +1,13 @@
 import subprocess
 
-result = subprocess.run(["vcgencmd","measure_temp"], capture_output = True, text = True)
+def runvcgencmd(command):
+    if ("measure_temp" == command):
+        return (subprocess.run(["vcgencmd","measure_temp"], capture_output = True, text = True)).stdout[5:9]
+    elif ("measure_volts" == command):
+        return (subprocess.run(["vcgencmd","measure_volts"], capture_output = True, text = True)).stdout[5:].strip("V\n")
+    elif ("get_throttled" == command):
+        return (subprocess.run(["vcgencmd","get_throttled"], capture_output = True, text = True)).stdout[10:].strip("\n")
+    elif ("measure_clock" == command):
+        return (subprocess.run(["vcgencmd","measure_clock", "arm"], capture_output = True, text = True)).stdout[13:].strip("\n")
 
-temp = float(result.stdout[5:9])
-
-print(result)
-print(temp)
+print(runvcgencmd("measure_temp"))
